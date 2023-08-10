f2c5438b4748d9e64ebfbfd563225795e8134ed0
Arsh234-GIT <ahamadarsh92@gmail.com>
2023-08-10 22:16:20 +0530

    add

--- src/Data.js
+++ src/Data.js
@@ -42,41 +42,41 @@ const Data = [
     ,
     {
       id: 4,
-      title: "Life Lessons with Katie Zaferes",
+      title: "Hidden Gems of Pink city & jaipur Hand Block painting",
       description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
-      price: 136,
-      coverImg: "image.png",
+      price: 60,
+      coverImg: "hm.jpg",
       stats: {
-        rating: 5.0,
+        rating: 4.9,
         reviewCount: 6
       },
-      location: "Online",
+      location: "Jaipur",
       openSpots: 0,
     },
     {
       id: 5,
-      title: "Learn Wedding Photography",
+      title: "Old Delhi's Food Temples &Spice Market",
       description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
-      price: 125,
-      coverImg: "weddingphotography.png",
+      price: 25,
+      coverImg: "igate.png",
       stats: {
-        rating: 5.0,
+        rating: 4.7,
         reviewCount: 30
       },
-      location: "Online",
-      openSpots: 27,
+      location: "Delhi",
+      openSpots: 59,
     },
     {
       id: 6,
-      title: "Group Mountain Biking",
+      title: "Old Delhi's Food & History A fusion of Taste & culture",
       description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
-      price: 50,
-      coverImg: "mountainbike.png",
+      price: 70,
+      coverImg: "old-d.png",
       stats: {
         rating: 4.8,
         reviewCount: 2
       },
-      location: "Norway",
+      location: "Delhi",
       openSpots: 3,
     }
   ];
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,9 +7,15 @@ class Navbar extends Component {
         return (
 
             <nav className="nav-container">
-                <img className="nav-logo" src="../Airbnb/Images/Vector.png" alt="airbnb logo" />
+                <img className="nav-logo" src="../Airbnb/Images/airbnb-logo.png" alt="airbnb logo" />
                 
-
+                <ul className="nav-list">
+                    <li>Home</li>
+                    <li>Sign up</li>
+                    <li> Log in</li>
+                  
+                    
+                </ul>
             </nav>
 
         )
